7dea783ecd8800a547c0634bd557d04ebe265f6c
Amir Khubeev <amir.hubeev@yandex.ru>
2026-03-04 19:05:20 +0300

    feat(backend): initial FastAPI setup with ruff and pre-commit

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get('/')
+def main_page():
+    return {'Hello': 'World'}
+
+
+@app.get('/items/{item_id}')
+def get_item(item_id: int):
+    return {'item_id': item_id}
