9d5d8c190e2b90c438933ab969d32f1662d3e233
Amir Khubeev <amir.hubeev@yandex.ru>
2026-03-10 11:55:31 +0300

    feat: создание моделей FastAPI

--- app/core/database.py
+++ app/core/database.py
@@ -1,17 +1,21 @@
-from sqlmodel import SQLModel, create_engine, Session
-from fastapi import Depends
 from typing import Annotated, Generator
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-connect_args = {"check_same_thread": False}
+from fastapi import Depends
+from sqlmodel import Session, SQLModel, create_engine
+
+sqlite_file_name = 'database.db'
+sqlite_url = f'sqlite:///{sqlite_file_name}'
+connect_args = {'check_same_thread': False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
+
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
+
 def get_session() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
+
 
 SessionDep = Annotated[Session, Depends(get_session)]
