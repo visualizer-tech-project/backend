15edc377b15026645a065845c80f154a90d5f84f
Amir Khubeev <amir.hubeev@yandex.ru>
2026-03-10 11:50:29 +0300

    feat: создание моделей в FastAPI

--- app/main.py
+++ app/main.py
@@ -1,23 +1,18 @@
-from fastapi import APIRouter, FastAPI
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from .core.database import create_db_and_tables
 
-from app.routers import (
-    career_tracks_router,
-    courses_router,
-    programs_router,
-    progress_router,
-    users_router,
-)
+app = FastAPI()
 
-app = FastAPI(title='EduMap API', version='1.0.0')
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
 
-api_v1_router = APIRouter(prefix='/api/v1')
+@app.get('/')
+def main_page():
+    return {'Hello': 'World'}
 
-api_v1_router.include_router(users_router)
-api_v1_router.include_router(programs_router)
-api_v1_router.include_router(courses_router)
-api_v1_router.include_router(career_tracks_router)
-api_v1_router.include_router(progress_router)
-
-app.include_router(api_v1_router)
-
-
+@app.get('/items/{item_id}')
+def get_item(item_id: int):
+    return {'item_id': item_id}
