f35eb309a286920e4c6440c77191120e2fb439dd
Amir Khubeev <amir.hubeev@yandex.ru>
2026-03-10 11:55:31 +0300

    feat: создание моделей FastAPI

--- app/main.py
+++ app/main.py
@@ -1,17 +1,22 @@
-from fastapi import FastAPI
 from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
 from .core.database import create_db_and_tables
 
 app = FastAPI()
 
+
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(_: FastAPI):
     create_db_and_tables()
     yield
+
 
 @app.get('/')
 def main_page():
     return {'Hello': 'World'}
+
 
 @app.get('/items/{item_id}')
 def get_item(item_id: int):
