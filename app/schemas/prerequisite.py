d8f6dfc4ca0bf0c335ab7b77e591bce0a63c2a5d
Amir Khubeev <hubeevamir@gmail.com>
2026-03-25 10:26:23 +0300

    feat: pydantic schemas (#13)
    Co-authored-by: Amir Khubeev <amir.hubeev@yandex.ru>

---
+++ app/schemas/prerequisite.py
@@ -0,0 +1,16 @@
+from pydantic import Field
+
+from app.schemas.base import BaseModelSchema, BaseSchema
+
+
+class PrerequisiteCreate(BaseSchema):
+    """Схема для создания пререквизита"""
+
+    prerequisite_id: int = Field(..., gt=0, description='ID курса-пререквизита')
+
+
+class PrerequisitePublic(BaseModelSchema):
+    """Публичная информация о пререквизите"""
+
+    course_id: int
+    prerequisite_course_id: int
